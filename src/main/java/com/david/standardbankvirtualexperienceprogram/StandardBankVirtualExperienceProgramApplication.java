95b39981000f90f422d203b07b921debca32a122
David <davidbazanticse@gmail.com>
2023-01-27 13:00:11 +0000

    First commit from Source Code.

---
+++ src/main/java/com/david/standardbankvirtualexperienceprogram/StandardBankVirtualExperienceProgramApplication.java
@@ -0,0 +1,13 @@
+package com.david.standardbankvirtualexperienceprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardBankVirtualExperienceProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandardBankVirtualExperienceProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/standardbankvirtualexperienceprogram/StandardBankVirtualExperienceProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.standardbankvirtualexperienceprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandardBankVirtualExperienceProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
